e4949dbf8125ee07ad3cf5c4877f9670585cf81b
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 19:49:00 +0400

    Open/Closed (Bad example)

---
+++ OpenClosed/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OpenClosed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    //Bad Example
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class Circle
+    {
+        public double Radius { get; set; }
+    }
+
+    public class Calculate
+    {
+        public double Area(object[] shapes)
+        {
+            double area = 0;
+            foreach (var shape in shapes)
+            {
+                if (shape is Rectangle r)
+                {
+                    area += r.Height * r.Width;
+                }
+                else if (shape is Circle c)
+                {
+                    area += c.Radius * c.Radius * Math.PI;
+                }
+            }
+
+            return area;
+        }
+    }
+
+
+}
