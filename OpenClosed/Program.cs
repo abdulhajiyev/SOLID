b8212d73f8af00f5af6c26f8c08d89a75c1fc9e5
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 20:54:33 +0400

    Good Example

--- OpenClosed/Program.cs
+++ OpenClosed/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OpenClosed
@@ -7,7 +8,16 @@ namespace OpenClosed
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Developer> developers = new List<Developer>
+            {
+                new Junior(0, "Filankes", 500),
+                new Senior(1, "Senior", 1500)
+            };
+
+            foreach (var developer in developers)
+            {
+                Console.WriteLine(developer.GetBonus());
+            }
         }
     }
 
@@ -46,33 +56,78 @@ namespace OpenClosed
             return area;
         }
     }*/
+    /* // Good Example
+        public abstract class Shape
+        {
+            public abstract double Area();
+        }
+
+        public class Rectangle : Shape
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public override double Area() => Width * Height;
+        }
+
+        public class Circle : Shape
+        {
+            public double Radius { get; set; }
+            public override double Area() => Radius * Radius * Math.PI;
+        }
+
+        public class Calculate
+        {
+            public double Area(Shape[] shapes)
+            {
+                return shapes.Sum(shape => shape.Area());
+            }
+        }*/
 
 
 
-    // Good Example
-    public abstract class Shape
+    public abstract class Developer
     {
-        public abstract double Area();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public abstract double GetBonus();
+
+        public Developer(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
     }
 
-    public class Rectangle : Shape
+    public class Junior : Developer
     {
-        public double Width { get; set; }
-        public double Height { get; set; }
-        public override double Area() => Width * Height;
+        public override double GetBonus() => Salary * 0.1;
+
+        public Junior(int id, string name, double salary) : base(id, name, salary) { }
     }
 
-    public class Circle : Shape
+    public class Senior : Developer
     {
-        public double Radius { get; set; }
-        public override double Area() => Radius * Radius * Math.PI;
+        public override double GetBonus() => Salary * 0.25;
+
+        public Senior(int id, string name, double salary) : base(id, name, salary) { }
     }
 
     public class Calculate
     {
-        public double Area(Shape[] shapes)
+        public double TotalBonus(Developer[] developers)
         {
-            return shapes.Sum(shape => shape.Area());
+            double totalBonus = 0;
+            foreach (var developer in developers)
+            {
+                Console.WriteLine(developer.GetBonus());
+                totalBonus = developer.GetBonus();
+            }
+
+            return totalBonus;
         }
     }
 }
