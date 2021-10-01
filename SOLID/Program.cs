7d86ca8501bee165f3fc1e5106f00bc91e314378
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 19:41:12 +0400

    good example

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -8,22 +8,43 @@ namespace SOLID
         {
             // SOLID
         }
-        
 
-        // Bad Example
+
+        /*// Bad Example
         public class SendEmail
         {
             public bool SendEmailForX(string input, string mailAddress)
             {
-                if (!IsMailAddressValid(mailAddress))
-                {
-                    return false;
-                }
-                return true;
+                return IsMailAddressValid(mailAddress);
             }
 
             private bool IsMailAddressValid(string mailAddress)
             {
+                // doSomething
+                return true;
+            }
+        }*/
+
+        // Good Example
+        public class MailAddressValid
+        {
+            public bool IsMailAddressValid(string mailAddress)
+            {
+                // doSomething
+                return true;
+            }
+            
+        }
+
+        public class SendEmail
+        {
+            public MailAddressValid validation { get; set; } = new MailAddressValid();
+            public bool SendEmailForX(string input, string mailAddress)
+            {
+                if (!validation.IsMailAddressValid(mailAddress))
+                {
+                    return false;
+                }
                 // doSomething
                 return true;
             }
