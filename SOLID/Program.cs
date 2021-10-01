e997a06839396c369aef06dab8089d5839fbf9f5
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 19:35:48 +0400

    Add project files.

---
+++ SOLID/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SOLID
+        }
+        
+
+        // Bad Example
+        public class SendEmail
+        {
+            public bool SendEmailForX(string input, string mailAddress)
+            {
+                if (!IsMailAddressValid(mailAddress))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            private bool IsMailAddressValid(string mailAddress)
+            {
+                // doSomething
+                return true;
+            }
+        }
+    }
+}
