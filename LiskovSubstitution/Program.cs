b1bc36210d31dedae602f881c76d0fc72ee70da8
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-01 20:59:35 +0400

    Liskov Bad Example

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LiskovSubstitution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    // Bad Example
+
+    public abstract class BasePrint
+    {
+        public abstract void Print();
+        public abstract void Scan();
+    }
+
+    public class HpPrinter : BasePrint
+    {
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Scan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CanonPrinter : BasePrint
+    {
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Scan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
